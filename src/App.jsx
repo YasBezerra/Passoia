bdc0eac47f109e614a68abffbd12713e3b8d5ca3
YasBezerra <yasmin.beatrizz@gmail.com>
2025-11-12 10:37:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+// arquivo principal
+// para escrita do nome do componente principal funcional usamos sempre a primeira letra maiuscula, PascalCase
+function App() {
+  return(
+    <>
+    <Header/>
+    </>
+  )
+}
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+
+
+function Header(){
+    return(
+        <header>
+            <img src="" alt="logo marca da pagina" />
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADE</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
